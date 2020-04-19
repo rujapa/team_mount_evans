4c96441c50eada141715b952611d75d631569e85
Stephen-Lipsky <stli1347@colorado.edu>
2020-04-18 20:32:36 -0600

    Initial ingestion steps.

--- spark_data_ingest.py
+++ spark_data_ingest.py
@@ -12,3 +12,17 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
+
+# Create Spark DataFrame & display first 10 rows
+tweet_raw = (spark.read.format("csv").options(header="true").load("20200312_Coronavirus_Tweets_Subset.CSV"))
+display(tweet_raw)
+tweet_raw.show(10, False)
+
+# Initially filter tweets in English
+# noinspection PyUnresolvedReferences
+from pyspark.sql.functions import explode
+
+tweet_filter = tweet_raw.select(tweet_raw['account_lang'] == 'en')
+tweet_filter.show(10, False)
+
+
